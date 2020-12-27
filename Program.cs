259955f30b0723c3951221d28ef4efddbfa3bc47
DeVante' Moore <devantemoore@DeVantes-MacBook-Pro.local>
2020-12-27 14:29:10 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace csharp_oop_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Animal{
+        public string Name {get; set;} // public properties & methods can be declared inside and out the class
+        public string Sound {get; set;}
+        private int Age {get; set;} // private properties & methods can only be declared INSIDE the class
+
+        public void SetAge(int age){
+            this.Age = age;
+        }
+    }
+}
